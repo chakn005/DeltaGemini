8d16c6a561f326d90a5c46d8044c77aa37f9448e
chakn005 <niloy.chakraborty@disney.com>
2026-06-02 07:02:04 +0000

    Deploying to gh-pages from @ chakn005/DeltaGemini@f9b65ff84c97ec4326f445896248d281d53fa3fb 🚀

--- shared/data.js
+++ shared/data.js
@@ -6,7 +6,7 @@ window.GEMINI_DATA = {
   "jira": {
     "baseUrl": "https://jira.disney.com",
     "browsePath": "/browse/",
-    "lastSynced": "2026-06-02T06:41:12Z",
+    "lastSynced": "2026-06-02T07:01:47Z",
     "syncSource": "scripts/sync-from-jira.py",
     "syncRequired": false,
     "syncTestPlans": [
@@ -24,7 +24,7 @@ window.GEMINI_DATA = {
       "fda-xavier": "RIGHTS-28225",
       "fda-falcon": "RIGHTS-28225"
     },
-    "dataVersion": "20260602064112"
+    "dataVersion": "20260602070147"
   },
   "flowSteps": [
     {
